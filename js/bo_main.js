9dbb010ade322f4de06fe290c136479505a4ad97
ESW1234 <seesangamdeepa@yahoo.com>
2016-03-30 11:32:01 -0700

    Update bo_main.js

--- js/bo_main.js
+++ js/bo_main.js
@@ -8,4 +8,4 @@ $Lightning.use("embeddedService:liveAgentSidebarApp", function() {
     chasitorContentServerEndpointURL : 'http://bochean-ltm2.internal.salesforce.com:8095/content',
     chasitorSrcURL : ['http://bochean-ltm2.internal.salesforce.com:6109/jslibrary/1446053165000/ui-sfdc-javascript/debug/SfdcCore.js','http://bochean-ltm2.internal.salesforce.com:8095/content/g/js/37.0/chasitor.js']
   }, "button_goes_here");
-});
+}, 'http://subbu-153c1e39e36.localhost.force.com:6109/esw0108');
