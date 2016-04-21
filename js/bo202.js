d48a8ac7749682c41d04ced7d1c3d65359ca6e0e
ESW1234 <seesangamdeepa@yahoo.com>
2016-04-21 14:36:25 -0700

    Update bo202.js

--- js/bo202.js
+++ js/bo202.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function(event) {
   var buttonDiv = document.createElement('div');
-  buttonDiv.setAttribute('id', 'button_goes_2here');
+  var buttonDivId = 'button_goes_here';
+  buttonDiv.setAttribute('id', buttonDivId);
   document.body.appendChild(buttonDiv);
   
   var ltngOutScript = document.createElement('script');
@@ -8,14 +9,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.body.appendChild(ltngOutScript);
   
   // esw@bo.org
-  $Lightning.use("embeddedService:liveAgentSidebarApp", function() {
-    $Lightning.createComponent("embeddedService:liveAgentSidebar", {
+  $Lightning.use('embeddedService:liveAgentSidebarApp', function() {
+    $Lightning.createComponent('embeddedService:liveAgentSidebar', {
       deploymentId : '572xx000000001x',
       orgId : '00Dxx0000001gMk',
       buttonId : '573xx000000001x',
       endpointURL : 'http://bochean-ltm2.internal.salesforce.com:8096/chat',
       chasitorContentServerEndpointURL : 'http://bochean-ltm2.internal.salesforce.com:8095/content',
       chasitorSrcURL : ['http://bochean-ltm2.internal.salesforce.com:6109/jslibrary/1446053165000/ui-sfdc-javascript-impl/debug/SfdcCore.js','http://bochean-ltm2.internal.salesforce.com:8095/content/g/js/37.0/chasitor.js']
-    }, "button_goes_here");
+    }, buttonDivId);
   }, 'http://subbu-153c1e39e36.localhost.force.com:6109/esw0108');
 });
