dcc87743d6961a416e7556bfc025b38294d53c13
ESW1234 <seesangamdeepa@yahoo.com>
2016-11-04 10:30:48 -0700

    Update mySite.js

--- igor/mySite.js
+++ igor/mySite.js
@@ -1,8 +1,8 @@
 var links = ['<a href="bl2index.html">DEFAULT WIDGET</a>', '<a href="bl2NoPrechat.html">NO PRECHAT</a>', '<a href="bl2Hidden.html">HIDDEN BUTTON</a>', '<a href="bl2NoWidget.html">NO WIDGET</a>', '<a href="bl2CustomText.html">CUSTOM BUTTON TEXT</a>', '<a href="bl2CustomCSS.html">CUSTOM STYLING</a>', '<a href="bl2Button2.html">BUTTON 2</a>', '<a href="bl2Link.html">BUTTON AS A LINK</a>', '<a href="bl2NoPrechatWithExtraPrechatFormDetails.html">NO PRECHAT WITH EXTRA PRECHAT FORM DETAILS</a>'];
 
 function openNav() {
-    document.getElementById("sideNav").style.width = "250px";
-    document.getElementById("text").style.marginLeft = "250px";
+    document.getElementById("sideNav").style.width = "195px";
+    document.getElementById("text").style.marginLeft = "197px";
     document.getElementById("link1").innerHTML = links[0];
     document.getElementById("link2").innerHTML = links[1];
     document.getElementById("link3").innerHTML = links[2];
