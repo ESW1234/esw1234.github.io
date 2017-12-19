840b94848f54052e91019c542961cd1d262876ab
ESW1234 <seesangamdeepa@yahoo.com>
2017-12-19 11:57:04 -0600

    Update login.js

--- jake/login.js
+++ jake/login.js
@@ -1,3 +1,51 @@
+/*
+ This login script wont work unless you override your headers to fake cross-origin. If you use requestly
+ (make sure you replace jakerouss-ltm1 with your app server):
+ 
+   {
+    "creationDate": 1513705693613,
+    "description": "",
+    "id": "Headers_1513705693613",
+    "name": "OAuth Response",
+    "pairs": [
+      {
+        "header": "Access-Control-Allow-Origin",
+        "source": {
+          "key": "Url",
+          "operator": "Contains",
+          "value": "http://jakerouss-ltm1.internal.salesforce.com:6109/services/oauth2/token"
+        },
+        "target": "Response",
+        "type": "Add",
+        "value": "http://esw1234.github.io"
+      },
+      {
+        "header": "Access-Control-Allow-Credentials",
+        "source": {
+          "key": "Url",
+          "operator": "Contains",
+          "value": "http://jakerouss-ltm1.internal.salesforce.com:6109/services/oauth2/token"
+        },
+        "target": "Response",
+        "type": "Add",
+        "value": "true"
+      },
+      {
+        "header": "Access-Control-Allow-Methods",
+        "source": {
+          "key": "Url",
+          "operator": "Contains",
+          "value": "http://jakerouss-ltm1.internal.salesforce.com:6109/services/oauth2/token"
+        },
+        "target": "Response",
+        "type": "Add",
+        "value": "HEAD, GET, POST, PUT, PATCH, DELETE"
+      }
+    ],
+    "ruleType": "Headers",
+    "status": "Active"
+  }
+*/
 (function() {
   const OAUTH_CLIENT_ID = "3MVG9AOp4kbriZOLiNgBjx.ukyB1nkyyeMSqElIwjT_cJg7P6FjIxyKYcvPFoubYhwesNHl6zUJdugbDETJMe";
   const OAUTH_CLIENT_SECRET = "15410901038364326";
@@ -8,10 +56,15 @@
     const request = new XMLHttpRequest();
 
     request.onload = function(e) {
-      const response = request.responseText; // not responseText
-      console.log(response)
+      const responseData = JSON.parse(request.responseText);
+      
+      if(request.status === 200) {
+        embedded_svc.auth.oauthToken = responseData.access_token;
+      } else {
+        console.log(`[Login] Unable to login: ${responseData.error} - ${responseData.error_description}.`);
+      }
     }
     request.open("POST", `http://jakerouss-ltm1.internal.salesforce.com:6109/services/oauth2/token?grant_type=password&client_id=${OAUTH_CLIENT_ID}&client_secret=${OAUTH_CLIENT_SECRET}&username=${username}&password=${password}`);
     request.send();
   };
-})();
+})()
