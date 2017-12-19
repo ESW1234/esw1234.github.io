7a7a1509dbad971f812f33062ae24928c1dc23e6
ESW1234 <seesangamdeepa@yahoo.com>
2017-12-19 11:51:35 -0600

    Create login.js

---
+++ jake/login.js
@@ -0,0 +1,17 @@
+(function() {
+  const OAUTH_CLIENT_ID = "3MVG9AOp4kbriZOLiNgBjx.ukyB1nkyyeMSqElIwjT_cJg7P6FjIxyKYcvPFoubYhwesNHl6zUJdugbDETJMe";
+  const OAUTH_CLIENT_SECRET = "15410901038364326";
+  document.querySelector("#login-button").onclick = () => {
+    const username = document.querySelector("#login-name").value;
+    const password = document.querySelector("#login-pass").value;
+
+    const request = new XMLHttpRequest();
+
+    request.onload = function(e) {
+      const response = request.responseText; // not responseText
+      console.log(response)
+    }
+    request.open("POST", `http://jakerouss-ltm1.internal.salesforce.com:6109/services/oauth2/token?grant_type=password&client_id=${OAUTH_CLIENT_ID}&client_secret=${OAUTH_CLIENT_SECRET}&username=${username}&password=${password}`);
+    request.send();
+  };
+})()
