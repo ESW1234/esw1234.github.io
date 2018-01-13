395db01c65dd15f62502d4cbafae63f762eeebc1
ESW1234 <seesangamdeepa@yahoo.com>
2018-01-12 17:54:43 -0800

    Make log file names unique enough to be valuable

--- jazmin/bin/logCLThenSync.sh
+++ jazmin/bin/logCLThenSync.sh
@@ -1,21 +1,19 @@
-#!/bin/bash
-#
-# NOTES: 
-# Assumes you are in main
-#
-# You must have an existing Directory in your home directory named CLDirectory
-# in order for this to work, however it will create the CL log file.  
-#
-#This will sync to the most recent CL so...
+#!/bin/bash                                                                                                                   
+#                                                                                                                             
+# NOTES:                                                                                                                      
+# Assumes you are in main                                                                                                     
+#                                                                                                                             
+# You must have an existing Directory in your home directory named CLDirectory                                                
+# in order for this to work, however it will create the CL log file.                                                          
+#                                                                                                                             
+#This will sync to the most recent CL so...                                                                                   
 
 
-# We want to have a separate log for each branch, finding name of file
-PATH_NAME=$(pwd)
-DIRNAME=$(basename $PATH_NAME)
-
-#append current CL to the end of CLLog for that branch
+# We want to have a separate log for each branch, finding name of file                                                        
+DIRNAME=$(pwd | sed 's/.*app\///' | sed 's/\//./')
+#append current CL to the end of CLLog for that branch                                                                        
 head changelist.blt >> ~/CLDirectory/$DIRNAME.CLLog.txt
 echo "" >> ~/CLDirectory/$DIRNAME.CLLog.txt
 
-#Sync to the latest CL for that branch
+#Sync to the latest CL for that branch                                                                                        
 blt --sync
