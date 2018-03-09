0f960f229a9d67f8aeaf85c511c604bfef9b9c60
ESW1234 <seesangamdeepa@yahoo.com>
2018-03-09 11:19:42 -0800

    Delete runComponentTests.sh

--- jeff/VSCode/.vscode/runComponentTests.sh
+++
@@ -1,5 +0,0 @@
-componentName=`basename "$1"`
-componentTestUrl="http://localhost:6109/embeddedService/$componentName.cmp?aura.mode=JSTESTDEBUG"
-
-echo "Now opening" $componentTestUrl
-open -a "/Applications/Google Chrome.app" "$componentTestUrl"
