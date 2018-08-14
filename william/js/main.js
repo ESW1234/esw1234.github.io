9c61b471f2947f38132620b1e788175453a2bb7d
William <william.chang@salesforce.com>
2018-08-14 15:55:31 -0700

    update path for util.js

--- william/js/main.js
+++ william/js/main.js
@@ -1,4 +1,4 @@
-requirejs(["/util"], function(util) {
+requirejs(["js/util"], function(util) {
     //This function is called when scripts/helper/util.js is loaded.
     //If util.js calls define(), then this function is not fired until
     //util's dependencies have loaded, and the util argument will hold
