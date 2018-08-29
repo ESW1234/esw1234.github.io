e43b4a827beed383fe091f707dc82da3d4e3fb0a
ESW1234 <seesangamdeepa@yahoo.com>
2018-08-29 10:09:16 -0700

    Update snapins.js

--- ashah/investigations/scripts/snapins.js
+++ ashah/investigations/scripts/snapins.js
@@ -15,4 +15,5 @@ embedded_svc.settings.language = ''; //For example, enter 'en' or 'en-US'
 //embedded_svc.settings.offlineSupportMinimizedText = '...'; //(Defaults to Contact Us)
 embedded_svc.settings.enabledFeatures = ['LiveAgent'];
 embedded_svc.settings.entryFeature = 'LiveAgent';
+embedded_svc.settings.devMode = true;
 embedded_svc.init('https://mokshith-dev-ed.my.salesforce.com', 'https://mohsaha-developer-edition.na46.force.com/', gslbBaseURL, '00Di0000000kajm', 'Chat_Deployment', { baseLiveAgentContentURL: 'https://c.la1-c2-iad.salesforceliveagent.com/content', deploymentId: '5720H0000006rq8', buttonId: '5730H0000006vzf', baseLiveAgentURL: 'https://d.la1-c2-iad.salesforceliveagent.com/chat', eswLiveAgentDevName: 'EmbeddedServiceLiveAgent_Parent04I0H000000TN61UAG_16326a5b2fa', isOfflineSupportEnabled: false}); };if (!window.embedded_svc) { var s = document.createElement('script'); s.setAttribute('src', 'https://mokshith-dev-ed.my.salesforce.com/embeddedservice/5.0/esw.min.js'); s.onload = function() { initESW(null); }; document.body.appendChild(s); } else { initESW('https://service.force.com'); }
