83b182a058f987d43c3b14b4cd999ee6b4e36d66
William <william.chang@salesforce.com>
2018-11-30 12:35:24 -0800

    exclude sidebar as well

--- william/stmfb/a3/inert_helper.js
+++ william/stmfb/a3/inert_helper.js
@@ -1,11 +1,16 @@
+/**
+ * This function controls toggles whether the background page is set as 
+ */
 function toggleInert() {
 	const isPageInert = embedded_svc.settings.isPageInert || false;
-	const num_children = document.body.children.length;
+	const numChildren = document.body.children.length;
+	const previousInertSettings = {}
 	console.log('Toggling inert status of every child element of the body to: ' + !isPageInert);
-	for (let i = 0; i < num_children; i++) {
+	for (let i = 0; i < numChildren; i++) {
 		const child = document.body.children[i];
 		if (['button_goes_here', 'inert-button'].indexOf(child.id) === -1 &&
-			child.className.indexOf('embeddedServiceHelpButton') === -1) {
+				child.className.indexOf('embeddedServiceHelpButton') === -1 &&
+				child.className.indexOf('embeddedServiceSidebar') === -1) {
 			child.inert = !isPageInert;
 		}
 	}
