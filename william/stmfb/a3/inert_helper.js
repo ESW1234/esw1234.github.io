76baa65126aa9ab13535b5e403db2118d75305d2
William <william.chang@salesforce.com>
2018-11-30 11:49:31 -0800

    update logging

--- william/stmfb/a3/inert_helper.js
+++ william/stmfb/a3/inert_helper.js
@@ -1,7 +1,7 @@
 function toggleInert() {
 	const inertEnabled = embedded_svc && embedded_svc.settings && embedded_svc.settings.inertEnabled;
 	const num_children = document.body.children.length;
-	console.log('Toggling inert status of every child element of the body to: ' + inertEnabled);
+	console.log('Toggling inert status of every child element of the body to: ' + !inertEnabled);
 	for (let i = 0; i < num_children; i++) {
 		const child = document.body.children[i];
 		if (['button_goes_here', 'inert-button'].indexOf(child.id) === -1) {
