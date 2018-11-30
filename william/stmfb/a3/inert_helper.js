8e6d175acabe64fc97cf807d5a02c6d59087a6c6
William <william.chang@salesforce.com>
2018-11-30 13:24:31 -0800

    add param

--- william/stmfb/a3/inert_helper.js
+++ william/stmfb/a3/inert_helper.js
@@ -1,19 +1,22 @@
 /**
- * This function controls toggles whether the background page is set as 
+ * This function controls toggles whether the background page is inert.
+ * 
+ * @param {boolean} [setInert] - Explicitly set inert.
  */
-function toggleInert() {
+function toggleInert(setInert) {
 	const isPageInert = embedded_svc.settings.isPageInert || false;
+	const shouldSetInert = (typeof setInert === 'boolean' && setInert) || !isPageInert;
 	const numChildren = document.body.children.length;
 	const previousInertSettings = {}
-	console.log('Toggling inert status of every child element of the body to: ' + !isPageInert);
+	console.log('Toggling inert status of every child element of the body to: ' + shouldSetInert);
 	for (let i = 0; i < numChildren; i++) {
 		const child = document.body.children[i];
 		if (['button_goes_here', 'inert-button'].indexOf(child.id) === -1 &&
 				child.className.indexOf('embeddedServiceHelpButton') === -1 &&
 				child.className.indexOf('embeddedServiceSidebar') === -1) {
-			child.inert = !isPageInert;
+			child.inert = shouldSetInert;
 		}
 	}
 
-	embedded_svc.settings.isPageInert = !isPageInert;
+	embedded_svc.settings.isPageInert = shouldSetInert;
 }
