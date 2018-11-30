50d62bc4f4bcc3b3b64f51cbb6ddf2d65f5b79e7
William <william.chang@salesforce.com>
2018-11-30 12:01:05 -0800

    inert.js fix to updating status

--- william/stmfb/a3/inert_helper.js
+++ william/stmfb/a3/inert_helper.js
@@ -1,11 +1,13 @@
 function toggleInert() {
-	const inertEnabled = embedded_svc && embedded_svc.settings && embedded_svc.settings.inertEnabled;
+	const isPageInert = embedded_svc.settings.isPageInert || false;
 	const num_children = document.body.children.length;
-	console.log('Toggling inert status of every child element of the body to: ' + !inertEnabled);
+	console.log('Toggling inert status of every child element of the body to: ' + !isPageInert);
 	for (let i = 0; i < num_children; i++) {
 		const child = document.body.children[i];
 		if (['button_goes_here', 'inert-button'].indexOf(child.id) === -1) {
-			child.inert = !inertEnabled;
+			child.inert = !isPageInert;
 		}
 	}
+
+	embedded_svc.settings.isPageInert = !isPageInert;
 }
