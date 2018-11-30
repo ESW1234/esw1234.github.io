8d1dc001dd8ace730619ac5f507c6b2d9ad57545
William <william.chang@salesforce.com>
2018-11-30 11:41:00 -0800

    update

---
+++ blitz-stmfb/a3/inert_helper.js
@@ -0,0 +1,11 @@
+function toggleInert() {
+	const inertEnabled = embedded_svc && embedded_svc.settings && embedded_svc.settings.inertEnabled;
+	const num_children = document.body.children.length;
+	console.log('Toggling inert status of every child element of the body to: ');
+	for (let i = 0; i < num_children; i++) {
+		const child = document.body.children[i];
+		if (['button_goes_here', 'inert-button'].indexOf(child.id) !== -1) {
+			document.body.children[i].inert = !inertEnabled;
+		}
+	}
+}
