484d05aa68c29ea895a5892e30ba89d96c2eccdd
ESW1234 <speddi@salesforce.com>
2020-01-08 15:29:44 -0800

    Create stmfb_init.js

---
+++ justin/steam/stmfb_init.js
@@ -0,0 +1,15 @@
+embedded_svc.init(
+	'https://snapins.my.stmfb.stm.salesforce.com',
+	'https://snapins-1600942b294.stmfb.stm.force.com/napili',
+	gslbBaseURL,
+	'00DRM0000005jYI',
+	'justin',
+	{
+		baseLiveAgentContentURL: 'https://c.la2-stmfb1-0-prd.stmfb.stm.salesforceliveagent.com/content',
+		deploymentId: '572RM0000004DDM',
+		buttonId: '573RM0000004DDR',
+		baseLiveAgentURL: 'https://d.la2-stmfb1-0-prd.stmfb.stm.salesforceliveagent.com/chat',
+		eswLiveAgentDevName: 'EmbeddedServiceLiveAgent_Parent04Ixx0000000001EAA_15ec5bd2971',
+		isOfflineSupportEnabled: true
+	}
+);
