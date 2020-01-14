759d6021d9e46df4beb6e3ea9f47f41151abef20
ESW1234 <speddi@salesforce.com>
2020-01-14 15:55:35 -0800

    Update main.js

--- justin/require/scripts/main.js
+++ justin/require/scripts/main.js
@@ -1,17 +1,19 @@
-requirejs(["util", "purify"], function(util, purify) {
+requirejs(["snippet"], function(snippet) {
     //This function is called when scripts/util.js is loaded.
     //If util.js calls define(), then this function is not fired until
     //util's dependencies have loaded, and the util argument will hold
     //the module value for "util".
-    console.log("require dependencies loaded");
+//     console.log("require dependencies loaded");
     
-    var script = document.createElement("script");
+//     var script = document.createElement("script");
     
-    script.type = "text/javascript";
-    script.src = "scripts/snippet.js";
-    script.onload = function() {
-        console.log("snippet loaded");
-    };
+//     script.type = "text/javascript";
+//     script.src = "scripts/snippet.js";
+//     script.onload = function() {
+//         console.log("snippet loaded");
+//     };
     
-    document.body.appendChild(script);
+//     document.body.appendChild(script);
+    
+    console.log("snippet loaded");
 });
