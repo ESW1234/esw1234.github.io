c963c3a2b1511273047481e4bd1b17223761ca1b
ESW1234 <speddi@salesforce.com>
2020-01-13 16:31:17 -0800

    Update main.js

--- justin/require/scripts/main.js
+++ justin/require/scripts/main.js
@@ -1,4 +1,4 @@
-requirejs(["snapins_snippet"], function(snippet) {
+requirejs(["util", "snippet"], function(util, snippet) {
     //This function is called when scripts/helper/snapins_snippet.js is loaded.
     //If snapins_snippet.js calls define(), then this function is not fired until
     //util's dependencies have loaded, and the util argument will hold
