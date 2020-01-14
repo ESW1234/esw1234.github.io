ccc46b20e13776d6aa72d457850d2b2283140320
ESW1234 <speddi@salesforce.com>
2020-01-13 16:32:00 -0800

    Update main.js

--- justin/require/scripts/main.js
+++ justin/require/scripts/main.js
@@ -1,6 +1,6 @@
 requirejs(["util", "snippet"], function(util, snippet) {
-    //This function is called when scripts/helper/snapins_snippet.js is loaded.
-    //If snapins_snippet.js calls define(), then this function is not fired until
+    //This function is called when scripts/helper/snippet.js is loaded.
+    //If snippet.js calls define(), then this function is not fired until
     //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/snapins_snippet".
+    //the module value for "snippet".
 });
