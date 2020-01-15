7de0621a24a8e2cf8d73ca62a604861a0e53b1a7
ESW1234 <speddi@salesforce.com>
2020-01-14 20:39:22 -0800

    Update util.js

--- justin/require/scripts/util.js
+++ justin/require/scripts/util.js
@@ -1,13 +1,4 @@
-// example module to load from require.html
 define({
     color: "black",
     size: "unisize"
 });
-console.log("util.js loaded - define called");
-// define(function() {
-//     var sample1 = {};
-//     //do your stuff
-//     return sample1;
-// });
-// // Just set a global var
-// myFoobar = "foo";
