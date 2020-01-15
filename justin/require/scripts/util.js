11e14cfc5468d5047287a870e7a4689944f1ad72
ESW1234 <speddi@salesforce.com>
2020-01-14 17:57:07 -0800

    Update util.js

--- justin/require/scripts/util.js
+++ justin/require/scripts/util.js
@@ -4,10 +4,10 @@ define({
     size: "unisize"
 });
 console.log("util.js loaded - define called");
-define(function() {
-    var sample1 = {};
-    //do your stuff
-    return sample1;
-});
-// Just set a global var
-myFoobar = "foo";
+// define(function() {
+//     var sample1 = {};
+//     //do your stuff
+//     return sample1;
+// });
+// // Just set a global var
+// myFoobar = "foo";
