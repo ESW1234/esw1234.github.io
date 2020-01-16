62aca710a2586dd061f240f99d4edaae08b55a8d
ESW1234 <speddi@salesforce.com>
2020-01-16 11:31:41 -0800

    Update snippet.js

--- justin/require/scripts/snippet.js
+++ justin/require/scripts/snippet.js
@@ -1,4 +1,4 @@
-console.log("snippet");
+console.log("inside snippet");
 var initESW = function(gslbBaseURL) {
       embedded_svc.settings.displayHelpButton = true; //Or false
       embedded_svc.settings.prepopulatedPrechatFields = {FirstName: "justin", LastName: "mac"}; //Sets the auto-population of pre-chat form fields
