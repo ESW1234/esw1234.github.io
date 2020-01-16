9558dea923c6317511a86ba84d3d485534109dbd
ESW1234 <speddi@salesforce.com>
2020-01-16 13:59:43 -0800

    Update anonDefine.js

--- justin/mismatchedAnonymousDefineExample/scripts/anonDefine.js
+++ justin/mismatchedAnonymousDefineExample/scripts/anonDefine.js
@@ -1,2 +1,5 @@
 // This file will be loaded NOT using require.js and before util.js to reproduce the mismatch anonymous define module error.
-define('anon', {});
+define({});
+
+// NOTE: if you comment out line 2 and include the below line instead the mismatch anonymous define error will be fixed.
+// define('anon', {});
