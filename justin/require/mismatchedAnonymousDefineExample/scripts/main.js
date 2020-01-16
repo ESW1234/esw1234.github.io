f49bef211804f772e66f8a58c0f4c82f6c8075fb
ESW1234 <speddi@salesforce.com>
2020-01-16 13:52:05 -0800

    Update main.js

--- justin/require/mismatchedAnonymousDefineExample/scripts/main.js
+++ justin/require/mismatchedAnonymousDefineExample/scripts/main.js
@@ -1,9 +1,21 @@
 requirejs(["util"], function(util) {
-  /*
-   * You will never reach this because an error will be thrown on loading util.js via require.
-   * 
-   * An error is thrown because require has been loaded and another javascript file was loaded
-   * that calls define() but was not loaded via require.
-   */
+
   console.log("Loaded util.js via require.");
 });
+
+
+requirejs(
+	["scripts/util"],
+	function(util) {
+		/*
+		 * You will not reach this because an error will be thrown on loading util.js via require.
+		 * 
+		 * An error is thrown because require has been loaded and another javascript file was loaded
+		 * that calls define() but was not loaded via require.
+		 */
+		console.log("Successfully loaded util.js using require.");
+	},
+	function(err) {
+		console.error("Error loading util.js using require: " + err);
+	}
+);
