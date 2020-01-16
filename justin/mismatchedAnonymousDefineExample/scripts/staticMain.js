e81934991ccc7a7a85881e84d991ae0093c703aa
ESW1234 <speddi@salesforce.com>
2020-01-16 14:05:46 -0800

    Create staticMain.js

---
+++ justin/mismatchedAnonymousDefineExample/scripts/staticMain.js
@@ -0,0 +1,15 @@
+requirejs(
+	["util"],
+	function(util) {
+		/*
+		 * You will not reach this because an error will be thrown on loading util.js via require.
+		 * 
+		 * An error is thrown because require has been loaded and another javascript file was loaded
+		 * that calls define() but was not loaded via require.
+		 */
+		console.log("Successfully loaded util.js using require.");
+	},
+	function(err) {
+		console.error("Error loading util.js using require: " + err);
+	}
+);
