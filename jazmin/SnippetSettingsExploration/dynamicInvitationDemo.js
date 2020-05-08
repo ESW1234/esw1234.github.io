5797a6ad16ee00122f3d996dfd475136869b8b60
ESW1234 <speddi@salesforce.com>
2020-05-07 17:49:25 -0700

    Update dynamicInvitationDemo.js

--- jazmin/SnippetSettingsExploration/dynamicInvitationDemo.js
+++ jazmin/SnippetSettingsExploration/dynamicInvitationDemo.js
@@ -17,17 +17,29 @@
 	};
 
 	DynamicInvitationDemo.prototype.generateInvitation = function() {
+		var invitationScript =  document.createElement("script");
 		var invitation = document.createElement("div");
 		var invitationStyle = document.createElement('style');
 
+		// This is the invitation API specified in the static resource
+		// In the actual implementation we should probably not have people need to include this in their
+		// static resource as I don't see what exactly they would want to customize in it. So the
+		// invitationScript specific logic will probs go away.
+		invitationScript.type = "text/javascript";
+		invitationScript.innerHTML = dynamicInvitationDemo.invitationAPIs;
+		// Append the script to the page
+		document.body.appendChild(invitationScript);
+		
 		//This is the invitation markup
 		invitation.innerHTML = dynamicInvitationDemo.invitationMarkup;
 		
 		// This is the invitation style
 		invitationStyle.type = "text/css";
+		
 		// This is the styles to apply to the invitation.
 		invitationStyle.innerHTML = dynamicInvitationDemo.styleTag;
 		
+		// Append the style to the markup
 		invitation.append(invitationStyle);
 		
 		
