0586633c6e8124f8da123b143c7fc8e85cdf154a
Justin <jmacmillin@salesforce.com>
2020-12-14 16:17:51 -0800

    Remove Justin 230 laptop directory.

--- justin/230/laptop/require/scripts/main.js
+++
@@ -1,9 +0,0 @@
-requirejs(
-	["scripts/util"],
-	function(util) {
-		console.log("Successfully loaded util.js using require.");
-	},
-	function(err) {
-		console.error("Error loading util.js using require: " + err);
-	}
-);
