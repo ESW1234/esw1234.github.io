4cb130f8f98a464170d5dc00619193dea69c19fa
Justin <jmacmillin@salesforce.com>
2021-07-27 14:05:21 -0700

    Justin remove laptop 234 directory.

--- justin/234/laptop/require/scripts/mainNoError.js
+++
@@ -1,9 +0,0 @@
-requirejs(
-	["util", "snippet"],
-	function(util, snippet) {
-		console.log("Successfully loaded require dependencies.");
-	},
-	function(err) {
-		console.log("ERROR: " + err);
-	}
-);
--- justin/234/laptop/require/scripts/util.js
+++
@@ -1,2 +0,0 @@
-console.log("util.js");
-define(function(require) {});
