7ff0e055520da598930e2b8fe6d57a9351378469
ESW1234 <speddi@salesforce.com>
2021-07-29 15:56:09 -0700

    Update bootstrap.js

--- justin/async/lwr/bootstrap.js
+++ justin/async/lwr/bootstrap.js
@@ -733,6 +733,7 @@
 			iframe.sandbox = "allow-scripts allow-same-origin allow-modals allow-downloads";
 			iframe.onload = () => {
 				log("Created an iframe to load the aura application.");
+				handleAfterAppLoad();
 			};
 
 			document.body.appendChild(iframe);
