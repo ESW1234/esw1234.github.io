a984cf65de6ec6aac23018ff95f6621ad705dea6
ESW1234 <speddi@salesforce.com>
2021-12-02 13:28:30 -0800

    Update main.js

--- 3rdpartyscripts/scripts/main.js
+++ 3rdpartyscripts/scripts/main.js
@@ -1,6 +1,6 @@
 requirejs(
 	// Pass in either "snippet_stmfb" or "snippet_stmfc"
-	["util", "snippet_stmfc"],
+	["util", "snippet_stmfb"],
 	function(util, snippetFileName) {
 		// Success callback.
 		console.log("Snippet loaded successfully using require.js.");
