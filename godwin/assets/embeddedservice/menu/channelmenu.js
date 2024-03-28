e256522954ed08847caf8bc11c4fbf0e8f560099
ESW1234 <pchung@salesforce.com>
2024-03-28 16:13:59 -0400

    Update channelmenu.js

--- godwin/assets/embeddedservice/menu/channelmenu.js
+++ godwin/assets/embeddedservice/menu/channelmenu.js
@@ -3654,7 +3654,7 @@
 	 */
 	embedded_svc.menu.initializeEmbeddedMessaging = function initializeEmbeddedMessaging(menuItemData, isMenuItem) {
 		var labelsLanguage = embedded_svc.menu.menuConfig.additionalSettings.labelsLanguage;
-		var baseURL = menuItemData.siteUrl;
+		var baseURL = 'https://godwinlaw.my.localhost.sfdcdev.site.com:6101/ESWMessagingWebExperienc1709839200606';
 
 		// Load bootstrap.js from MIAW deployment's LWR site endpoint.
 		embedded_svc.utils.loadScriptFromUrl(
@@ -3692,7 +3692,7 @@
 				embeddedservice_bootstrap.init(
 					embedded_svc.menu.settings.orgId,
 					menuItemData.channel,
-					menuItemData.siteUrl,
+					'https://godwinlaw.my.localhost.sfdcdev.site.com:6101/ESWMessagingWebExperienc1709839200606',
 					{
 						scrt2URL: menuItemData.scrt2Url
 					}
