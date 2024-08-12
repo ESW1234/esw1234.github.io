93ae1995bff954d1767705d6715fb131644b925f
ESW1234 <pchung@salesforce.com>
2024-08-12 10:41:22 -0500

    Update lo-lwr-application.js

--- ecs/lo2/lo-lwr-application.js
+++ ecs/lo2/lo-lwr-application.js
@@ -1,42 +1,27 @@
 (function(that) {
-    'use strict';
+    "use strict";
 
     class LightningOut extends HTMLElement {
-        #frameDomain = 'https://dsg00000axdtj2a3.test1.my.pc-rnd.site.com/lp/lo2?embeddedCmp=fragment/z4vef6fpe0h7wnpytaj5p1600u5elax6i6m7rq364wp';
-        #framePath = '/';
+        iframeRef;
+        lastWidth;
+        ready = false;
 
-        #iframeRef;
-        #shadow;
-
-        #lastWidth;
-        #ready = false;
-        #messageListener;
-
-        #preload(endpoint, parentDomElement) {
-            const iframe = that.document.createElement('iframe');
-            const shadow = parentDomElement.attachShadow({ mode: 'closed' });
-            this.#shadow = shadow;
-            iframe.name = 'lightning_af';
-            iframe.sandbox = 'allow-same-origin allow-downloads allow-forms allow-scripts';
+        preload(endpoint, parentDomElement) {
+            const iframe = that.document.createElement("iframe");
+            const shadow = parentDomElement.attachShadow({ mode: "closed" })
+            iframe.id = "lightning_af";
+            iframe.name = "lightning_af";
+            iframe.scrolling = "no" // USE style="overflow:hidden;"
+            iframe.sandxox = "allow-downloads allow-forms allow-scripts";
             iframe.frameborder = 0;
-            iframe.scrolling = 'no' // USE style='overflow:hidden;'
-            iframe.style = 'width:100%;position:relative;border:0;padding:1px;overflow:none;visibility:none;background-color:#fffcb5';
-            iframe.onerror = () => that.alert('Error Loading <iframe> for ' + iframe.src);
+            iframe.style.width = '100%';
+            iframe.style = "position:relative;border:0;padding:1px;overflow:none;visibility:none;background-color:#FFFCB5;"
+            iframe.onerror = () => alert("Error Loading <iframe> for " + iframe.src);
             iframe.onload = (event) => {
-                if (parentDomElement.#ready) {
-                    // Someone is trying to change the page
-                    this.#shadow.innerHTML = '';
-                    this.remove();
-                    throw new Error('Lightning Out: This component cannot be rerendered for security reasons.');
-                }
-                iframe.style.display = 'block';
-                parentDomElement.#ready = true;
+                this.iframeRef = event.target;
+                this.iframeRef.style.display = 'block';
+                this.adjustIFrameSize();
             };
-
-            const title = parentDomElement.getAttribute('title');
-            if (title) {
-                iframe.title = title;
-            }
 
             // (1) Ensure that event.origin is set for all postMessage events.
             // (3) For events from the iframe to the host document. We will need to do the reverse.
@@ -45,36 +30,29 @@
             //     This flow is trickier as we do not necessarily know the URL of the host document 
             //     from the iframe. However, from the iframe back to the host document is slightly less of
             //     a concern and we could set the targetOrigin to '*' for now and fix this up later.
+            // function compareOrigin(eventOrigin) {
+            //     // TODO
+            // }
 
-            this.#messageListener = (event) => {
-                if (event.origin !== parentDomElement.#frameDomain) {
-                    that.console.trace(`Lightning Out: Message ignored due to different ${event.origin}.`);
-                    return;
-                }
-                if (event.source !== parentDomElement.#iframeRef.contentWindow) {
-                    that.console.trace('Lightning Out: Message ignored due to different source.');
-                    return;
-                }
+            window.addEventListener("message", (event) => {
                 switch(event.data.type) {
-                    case 'lo.container-sized': {
+                    case "lo.container-sized":
                         if (event.data) {
-                            parentDomElement.#iframeRef.height = event.data.height;
+                            this.iframeRef.height = event.data.height;
                         }
                         break;
-                    }
-                    case 'lo.dispatchEvent': {
-                        const customEvent = new that.CustomEvent(event.data.name, { detail: event.data.detail });
-                        parentDomElement.#dispatchEventComponent(customEvent);
+                    case 'lo.dispatchEvent':
+                        const customEvent = new CustomEvent(event.data.name, { detail: event.data.detail });
+                        parentDomElement.dispatchEventComponent(customEvent);
                         break;
-                    }
-                    case 'lo.ready': {
+                    case "lo.ready":
                         parentDomElement.addEventListener('ShowRecaptcha', () => {
-                            const form = that.document.getElementById('form');
+                            const form = document.getElementById('form');
                             form.style.display = 'inline-block';
-                            const button = that.document.getElementById('button');
-                            button.addEventListener('click', () => {
-                                const inputEl = that.document.getElementById('returnInput');
-                                const customEvent = new that.CustomEvent('returnValue', {
+                            const button = document.getElementById('button');
+                            button.addEventListener("click", () => {
+                                const inputEl = document.getElementById('returnInput');
+                                const customEvent = new CustomEvent('returnValue', {
                                     detail: {
                                         returnValue: inputEl.value
                                     }
@@ -84,69 +62,56 @@
                             });
                         });
                         break;
-                    }
                 }
-                event.stopImmediatePropagation();
-            };            
-            that.addEventListener('message', this.#messageListener);
+            });
 
+            new ResizeObserver(this.adjustIFrameSize).observe(parentDomElement);
+
+            shadow.appendChild(iframe);
             iframe.src = endpoint;
-            parentDomElement.#iframeRef = shadow.appendChild(iframe);
         }
 
-        #adjustIFrameSize() {
+        adjustIFrameSize() {
             // Set Max-Width for LWR Container
             const { offsetWidth, offsetHeight } = this;
-            if (this.#lastWidth !== offsetWidth) {
-                this.#lastWidth = offsetWidth;
-                this.#iframeRef.width = offsetWidth;
-                this.#iframeRef.contentWindow.postMessage({
+            if (this.lastWidth !== offsetWidth) {
+                this.lastWidth = offsetWidth;
+                this.iframeRef.width = offsetWidth;
+                this.iframeRef.contentWindow.postMessage({
                     type: 'lo.wrapper-size',
                     width: offsetWidth,
                     height: offsetHeight,
-                }, this.#frameDomain);
+                }, '*');
             }
-        }
-
-        #dispatchEventComponent() {
-            super.dispatchEvent(...arguments);
         }
 
         addEventListener(eventName) {
             super.addEventListener(...arguments);
-            this.#iframeRef.contentWindow.postMessage({
+            this.iframeRef.contentWindow.postMessage({
                 name: eventName,
                 type: 'lo.addEventListener',
-            }, this.#frameDomain);
+            }, '*');
         }
 
         dispatchEvent(event) {
             super.dispatchEvent(...arguments);
-            this.#iframeRef.contentWindow.postMessage({
+            this.iframeRef.contentWindow.postMessage({
                 name: event.type,
                 detail: event.detail,
                 type: 'lo.dispatchEvent',
-            }, this.#frameDomain);
+            }, '*');
+        }
+
+        dispatchEventComponent() {
+            super.dispatchEvent(...arguments);
         }
 
         adoptedCallback() {
             this.remove();
-            throw new Error('Lightning Out: This component cannot be rerendered for security reasons.');
         }
         connectedCallback() {
-            if (!this.#shadow) {
-                this.#preload(this.#frameDomain, this);
-            } else {
-                this.remove();
-                throw new Error('Lightning Out: This component cannot be rerendered for security reasons.');
-            }
-        }
-        disconnectedCallback() {
-            this.#shadow.innerHTML = '';
-            if (this.#messageListener) {
-                that.removeEventListener('message', this.#messageListener);
-            }
+            this.preload("https://dsg00000axdtj2a3.test1.my.pc-rnd.site.com/lp/lo2?embeddedCmp=fragment/z4vef6fpe0h7wnpytaj5p1600u5elax6i6m7rq364wp", this)
         }
     }
-    that.customElements.define('lo-lwr-application', LightningOut);
-})(window);
+    customElements.define("lo-lwr-application", LightningOut);
+})(this);
